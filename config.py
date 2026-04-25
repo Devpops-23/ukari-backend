91a87ed6c2739183b04ce56418488f3265ee447c
Robert <rtmensah35@yahoo.com>
2026-04-24 23:17:28 -0500

    Update config.py with DATABASE_URL and env fixes

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 BASE_DIR = Path(__file__).resolve().parent
 ENV_PATH = BASE_DIR / ".env"
 load_dotenv(dotenv_path=ENV_PATH)
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 # JWT
 SECRET = os.getenv("JWT_SECRET")
