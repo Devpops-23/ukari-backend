d924bbe18327b68747a105750716c18d0d063d02
Robert <rtmensah35@yahoo.com>
2026-04-24 22:59:22 -0500

    Fix circular import in db.py

--- db_utils/db.py
+++ db_utils/db.py
@@ -1,21 +1,12 @@
-# db_utils/db.py
-
-from db_utils.db import get_db
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base, Session
-from datetime import datetime
+from sqlalchemy.orm import sessionmaker, declarative_base
+from config import DATABASE_URL
 
-DATABASE_URL = "sqlite:///./ukari.db"
-
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False},
-)
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-
 
 def get_db():
     db = SessionLocal()
