a0d22232e0632ba8d2b2232390af80f4bd6da465
Robert <rtmensah35@yahoo.com>
2026-06-08 02:11:50 -0500

    update models.py

--- db_utils/models.py
+++ db_utils/models.py
@@ -12,7 +12,7 @@ from sqlalchemy import (
 from sqlalchemy.orm import relationship
 
 from db_utils.db import Base
-from traveler_app import get_order
+
 
 
 # ---------------------------------------------------------
